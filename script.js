46d2b26fd66eb11bd0ffddebe842ad7eba8f5ea5
sbrycbc <sabriyecbc@gmail.com>
2023-04-18 15:12:43 +0200

    added word list und updated style..

---
+++ script.js
@@ -0,0 +1,25 @@
+const word_el = document.getElementById('word');
+function getRandomWord(){
+    const words = ["javascript", "java", "python"];
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+const correctLetters = ['j','a'];
+const wrongLetters = [];
+function displayWord(){
+    const selectedWord = getRandomWord()
+
+    word_el.innerHTML = `
+    ${selectedWord.split('').map(letter => `
+    <div class="letter">
+    ${correctLetters.includes(letter) ? letter : ''}</div>`).join('')}
+    `;
+
+    const w = word_el.innerText.replace(/\n/g,'')
+    if(w === selectedWord) {
+        console.log(`you know!!`)
+    }
+}
+
+displayWord()
+
