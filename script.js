3d75dba34a433e3130ec8240fa1e89a7711cb251
sbrycbc <sabriyecbc@gmail.com>
2023-04-18 17:56:35 +0200

    added Wrong Letters...

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container');
-const massage = document.getElementById('success-message')
+const massage = document.getElementById('success-message');
+const wrongLetters_el = document.getElementById('wrong-letters');
+const items = document.querySelectorAll('.item')
+
 function getRandomWord(){
     const words = ["javascript", "java", "python"];
     return words[Math.floor(Math.random() * words.length)]
@@ -24,6 +27,23 @@ function displayWord(){
     }
 }
 
+function updateWrongLetters(){
+    wrongLetters_el.innerHTML = `
+    ${wrongLetters.length>0?'<h3>Wrong Letters</h3>':''}
+    ${wrongLetters.map(letter => `<span>${letter}<span>`)}
+    `;
+
+    items.forEach((item,index) => {
+        const errorCount = wrongLetters.length;
+
+        if (index<errorCount) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+}
+
 window.addEventListener('keydown', function(e){
     if ( e.keyCode >=65 && e.keyCode <=90){
         const letter = e.key;
@@ -39,7 +59,7 @@ window.addEventListener('keydown', function(e){
         }else{
             if(!wrongLetters.includes(letter)){
                 wrongLetters.push(letter);
-                console.log('update wrong letters!!')
+                updateWrongLetters()
             }
         }
     }
